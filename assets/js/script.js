c8b94111f219b51b851824fda6574c0f982c22b5
GortariCL <fernando.gortari@gmail.com>
2022-01-20 21:07:52 -0300

    Desafío Terminado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,50 @@
 $(document).ready(function () {
+    //Evento botón calcular
+    $('#calcular').on('click', function () {
+        //Captura de dato númerico del input
+        let num = document.getElementById('input-number').value;
 
-    let num = prompt("ingrese número");
+        //Validación rango númerico
+        if (num > 0 && num < 20) {
 
-    function multiplicacion(num) {
-        for (let i = 0; num > i; i++) {
-            var mult = (num * (i + 1));
-            console.log(mult);
-        }
-    }
-    multiplicacion(num);
-
-    function factorial(num) {
-        for (let i = 1; i <= num; i++) {
-            let resultado = 1;
-            for (let j = 1; i >= j; j++) {
-                resultado = resultado * j;
+            //Ciclo for para multiplicación
+            let multiplicacion = (num) => {
+                console.log(`MULTIPLICACIÓN DE ${num} ES:`);
+                for (let i = 0; num > i; i++) {
+                    var mult = (num * (i + 1));
+                    console.log(mult);
+                }
             }
-            console.log("Factorial de " + i + " es: " + resultado);
+            //Muesta de datos
+            multiplicacion(num);
+            console.log("\nFACTORIAL:");
+
+            //Ciclo for anidado Factorial
+            let factorial = (num) => {
+                for (let i = 1; i <= num; i++) {
+                    let resultado = 1;
+                    for (let j = 1; i >= j; j++) {
+                        resultado = resultado * j;
+                    }
+                    console.log(`Factorial de ${i} es: ${resultado}`);
+                }
+            }
+            //Muestra de datos y limpieza del imput
+            factorial(num);
+            console.log("\n")
+            document.getElementById('input-number').value = "";
+        //Else de validación rango numérico y limpieza del input    
+        } else {
+            alert("Número fuera de rango");
+            document.getElementById('input-number').value = "";
         }
-    }
-    factorial(num);
+    });
+
+    //Tooltips Bootstrap
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+
 
 });
-
-
-
-
